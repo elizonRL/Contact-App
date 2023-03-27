ce0128b57f5cedb144d63d172030ba0a883937b8
elizo <elizo@DESKTOP-0J85MTD>
2023-03-27 16:08:17 -0400

    Archivos Documentados

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+/**
+* JS Para Manejar los Eventos el formulario y losdatos motrados en pnatalla 
+* @author Elizon Rodriguez <elizorodriguez16@gmail.com>
+* @link https://github.com/mastermindac/frontend_html_css_js GitHub
+*/
 let nombre;
 let form;
 let error;
@@ -7,10 +12,18 @@ let contactos = []
 let id =0;
 let tr;
 let edit;
-
+/** 
+* Funcion que crea un metodo que solo acepta numeros en el imput de numero de telefono.
+* @param {event} event - el emvento enviado por el input del formulario.
+* @return {false} si ingresas letras retorna falso.
+*/
 function soloNumero(event){
     if (event.keyCode < 45 || event.keyCode > 57) event.returnValue = false;
 }
+/** 
+* Maneja el forulario para que el campo nombre y numero de telefono no esten vacios 
+* @param {event} event - maneja el evento sunmit del formulario.
+*/
 function comprobarFrom(event){
     event.preventDefault();
     if(nombre.value.length == 0){
@@ -25,38 +38,57 @@ function comprobarFrom(event){
         error.innerText = "El Numero es Muy Corto";
         telefono.focus();
         return false;
-    }
-    
-    agregarData();
-    agregarTabla();
-    
-}
+    }else{
+        agregarData();
+        agregarTabla();
+        nombre.value="";
+        email.value="";
+        telefono.value=""
+        nombre.focus();
+    }  
+/** 
+* Generador de ID'S Unicos 
+* @return {substring} Brief description of the returning value here.
+*/}
+
 generarIdUnico1 = () => { 
     return Math.random().toString(30).substring(2);           
-} 
-function agregarData(){
-    while(id<contactos.length){
-        
-        id++
-    }
-    contacto ={
-        "id": generarIdUnico1(),
-        "nombre": nombre.value,
-        "email": email.value,
-        "telefono": telefono.value
-    }
-    contactos.push(contacto);    
 }
-function editInfo(e){
-    
-    //console.log(e.target.classList);
-    if(e.target.classList.contains("delet")){
-        const deletId = e.target.getAttribute("data-id");
-        contactos= contactos.filter(contacto => contacto.id != deletId);
-        console.log(contactos);
+/** 
+* Crea un objeto con los datos Del contacto agregado y lo agrega a un array.
+* @param {string} contactos - Es un arreglo para guardar el objeto de JS.
+* @return {Array} Devuelve un Array de Objetos.
+*/ 
+1
+function agregarData(){
+    var contacto ={
+            "id": generarIdUnico1(),
+            "nombre": nombre.value,
+            "email": email.value,
+            "telefono": telefono.value
+        }
+    contactos.push(contacto);   
+} 
+/** 
+* Elimina un Elemento ciando se preciona el boton Delet de la tabla de comtactos.
+* @param {event} event- Maneja el evento click del elemento a creado en la funcion agregarTabla y elimina esa table row.
+*/
+
+function deletInfo(event){
+    event.preventDefault();
+    console.log(event.target.classList);
+    if(event.target.classList.contains("fa-trash-can")){
+        const deletId = event.target.getAttribute("data-id");
+        contactos= contactos.filter(contacto => contacto.id !== deletId);
+        console.log(deletId);
         agregarTabla();
     }
 }
+/** 
+* Crea La tabla HTML de forma Dinamica al momento de guardar el el contacto.
+* @param {Array} contactos - Trae todo los contacos guardados sobre el Array.
+*/
+
 function agregarTabla(){
     tr.innerHTML =" ";
     for(contactoitem of contactos){
@@ -66,17 +98,23 @@ function agregarTabla(){
             <td>${contactoitem.nombre}</td>
             <td>${contactoitem.email}</td>
             <td>${contactoitem.telefono}</td>
-            <td><input type="submit"  value="Edit" class="edit" ></td>
-            <td><input type="submit"  value="Delet" class="delet" data-id="${contactoitem.id}"></td>
+            <td><a href="" class="edit">
+            <i class="fa-solid fa-pen-to-square" data-id="${contactoitem.id}"></i>
+            </a></td>
+            <td><a href="" class="delet">
+            <i class="fa-solid fa-trash-can" data-id="${contactoitem.id}"></i>
+            </a></td>
             </tr>
         `;
         
     }
     edit = document.getElementById("edit");
-    tr.addEventListener("click", editInfo);
+    tr.addEventListener("click", deletInfo);
 }
 
-
+/** 
+* Verifica si el Dom Esta cargado y crea todo .
+*/
 function domCagado(){
     nombre = document.getElementById("name");
     form = document.getElementById("form");
@@ -89,5 +127,7 @@ function domCagado(){
     form.addEventListener("submit", comprobarFrom);
     
 }
-
+/** 
+* Escucha el evento DOMContentLoaded para verificar que todo esta cargado
+*/
 document.addEventListener("DOMContentLoaded", domCagado)
