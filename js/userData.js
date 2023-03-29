84f822c8582a296b03b0ed66ce3525c82db22e70
elizo <elizo@DESKTOP-0J85MTD>
2023-03-29 17:00:40 -0400

    userData comentada

--- js/userData.js
+++ js/userData.js
@@ -1,13 +1,19 @@
  /**
 * @author Elizon Rodriguez <elizorodriguez16@gmail.com>
 */
-
  let contacto;
-
+/** 
+* Optine los Datos que ingrese los usuarios.
+* @param {Object} contactos - Objeto con la informacion del contacto.
+*/
 function guardarDatos(contactos){
     localStorage.setItem("contacto", JSON.stringify(contactos))
 }
-
+/** 
+* Optiene Los datos desde el LocalStorage.
+* @param {} contacto 
+* @return {contacto} retorna un cobjeto con la informacion de contacto.
+*/
 function optenerDatos(){
     contacto = JSON.parse(localStorage.getItem("contacto"));
     console.log(contacto);
